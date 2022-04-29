4dbb6be06042dc91a85df2a854a4da9508fc6db0
Neil Mehra <neilmehra@outlook.com>
2022-04-29 02:07:05 -0400

    update handler

--- git_folder_handler.py
+++ git_folder_handler.py
@@ -1,22 +1,20 @@
 import os
 
+num_chunks = 4
+
 folders = [folder for folder in os.listdir('.') if folder[0] != '.']
-amt = len(folders)
-n = 4
-qrtr = amt // n
-# print('chunk size:', qrtr)
-remainder = amt % n
-if remainder > 20: raise Exception
+qrtr = len(folders) // num_chunks
+if qrtr > 20: raise Exception
 
 chunks = []
-for f in range(n): chunks.append(folders[qrtr*f:qrtr*(f+1)])
+for f in range(num_chunks): chunks.append(folders[qrtr*f:qrtr*(f+1)])
 chunks.append(folders[qrtr*4:])
 
 for i,chunk in enumerate(chunks):
     for ln in chunk:
         os.system('git add "{}"'.format(ln))
     os.system('git commit -m "chunk #{}, updating the following: {}'.format(i,chunk))
-    os.system('git push')
-    inp = input('Continue? Y/N ').strip().split()[0].lower()
+    os.system('git push origin master')
+    inp = input('Continue? Y/N ').strip()[0].lower()
     if inp == 'n': break
 
